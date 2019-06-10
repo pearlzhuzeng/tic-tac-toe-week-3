a7acb4720b574f6bf05c5e900c8abd9c6ea16768
Pearl Zeng <zhuzeng@umich.edu>
2019-06-10 13:13:13 -0400

    Make interface_spec 'invalid input' pass

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,6 +1,8 @@
 require_relative '../app/tic_tac_toe_board'
 
 class Interface
+  VALID_INPUT = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
+
   attr_reader :board
   attr_accessor :positions
 
@@ -12,12 +14,18 @@ class Interface
   def run(commands)
     commands.each_with_index do |command, i|
       player = i % 2 == 0 ? 'x' : 'o'
-      position = command.values.first.to_i
-      @board.mark(player, position)
+      position = command.values.first
+
+      if VALID_INPUT.include?(position)
+        @board.mark(player, position.to_i)
+      else
+        @positions[0] = "INVALID"
+      end
     end   
   end
 
   def next_message
+    return "That's not how we do things around here" if @positions.include?("INVALID")
     return 'MEOW MEOW, MOTHERFUCKER!' if @board.draw?
     return "#{@board.winner} wins" if @board.winner
     
